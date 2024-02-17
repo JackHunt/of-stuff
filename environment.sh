eaf02e2de73cf4caaa60b6eeaf994b7ca6c738af
Jack Hunt <jackmileshunt@gmail.com>
2024-02-17 07:57:40 +0000

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -10,4 +10,20 @@ if [[ "$(docker images -q $IMAGE_NAME:$IMAGE_TAG 2> /dev/null)" == "" ]]; then
   docker build -t $IMAGE_NAME:$IMAGE_TAG .
 fi
 
-docker run -it --rm -v "$PROJECTS_DIR:/projects" $IMAGE_NAME:$IMAGE_TAG
+# Taken from
+# https://github.com/ybycode/docker-openframeworks/blob/master/run.sh
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run -it --rm \
+  -v "$PROJECTS_DIR:/projects" \
+  --volume=$XSOCK:$XSOCK \
+  --volume=$XAUTH:$XAUTH \
+  --env="XAUTHORITY=${XAUTH}" \
+  --env="DISPLAY" \
+  --device=/dev/dri:/dev/dri \
+  --volume=/run/user/$(id -u)/pulse:/run/pulse \
+  --volume=$(pwd)/of:/of \
+  $IMAGE_NAME:$IMAGE_TAG
